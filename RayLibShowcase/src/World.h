c3ae7e9d580368fc3f99618a4401cfbb4e85e500
tikhonsmovzh <tikhonsmovzh@gmail.com>
2023-04-29 17:08:21 +0300

    design

--- RayLibShowcase/src/World.h
+++ RayLibShowcase/src/World.h
@@ -11,32 +11,12 @@
 #include <vector>
 
 class World {
-	int mScreenWidth = 800;
-	int mScreenHeight = 450;
-
-	std::vector<std::pair<cpVect, cpVect>> mFloorEnds{};
-	std::vector<cpShape*> mFloors{};
-	cpSpace* mSpace;
-	cpShape* mBallOne;
-	cpShape* mBallTwo;
-
-	cpBody* mBallBodyOne;
-	cpBody* mBallBodyTwo;
-	Color playerColor;
 public:
 	World();
 
 	Vector2 physicsToWorldCoord(float x, float y);
 
-	~World();
-
 	void update();
-};
-
-enum COLLISION_TYPES{
-	PLAYER,
-	OBJECT,
-	WALL
 };
 
 #endif //RAYLIBSHOWCASE_WORLD_H
--- RayLibShowcase/src/main.cpp
+++ RayLibShowcase/src/main.cpp
@@ -2,35 +2,25 @@
 #include <raylib.h>
 
 int main() {
-	// Initialization
-	//--------------------------------------------------------------------------------------
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+    Vector2 screen {1500, 850};
 
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - mouse input");
+	InitWindow(screen.x, screen.y, "game");
+	SetTargetFPS(60);
+    SetExitKey(KEY_TAB);
 
-	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-	//---------------------------------------------------------------------------------------
 	auto w = World();
-	// Main game loop
-	while (!WindowShouldClose())    // Detect window close button or ESC key
-	{
-		// Update
-		//----------------------------------------------------------------------------------
 
-		//----------------------------------------------------------------------------------
+	while (!WindowShouldClose()) {
+        BeginDrawing();
 
-		// Draw
-		//----------------------------------------------------------------------------------
-		w.update();
+        ClearBackground(WHITE);
 
-		//----------------------------------------------------------------------------------
-	}
+        w.update();
 
-	// De-Initialization
-	//--------------------------------------------------------------------------------------
-	CloseWindow();        // Close window and OpenGL context
-	//--------------------------------------------------------------------------------------
+        EndDrawing();
+    }
+
+	CloseWindow();
 
 	return 0;
 }
