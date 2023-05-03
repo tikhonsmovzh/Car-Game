175791fdbac7eea57cdf19b937ac0271db1aa63c
tikhonsmovzh <tikhonsmovzh@gmail.com>
2023-05-03 08:09:52 +0300

    add new button

---
+++ RayLibShowcase11/src/Button.cpp
@@ -0,0 +1,54 @@
+//
+// Created by tikho on 03.05.2023.
+//
+
+#include "Button.h"
+
+Button::Button(Rectangle rect, std::string text, Font TextFont, int font, Color col) {
+    rectangle = rect;
+
+    fontSize = font;
+
+    startPos = {rectangle.x, rectangle.y};
+
+    rectangle.x -= rectangle.width / 2;
+    rectangle.y -= rectangle.height / 2;
+
+    drawRectangle = rectangle;
+
+    this->text = text;
+    this->TextFont = TextFont;
+
+    this->col = col;
+}
+
+void Button::update(Vector2 camera) {
+    Color drawCol = col;
+    isTouch = false;
+
+    if (!CheckCollisionPointRec({(float)GetMouseX() + camera.x, (float)GetMouseY() + camera.y}, drawRectangle)) {
+        if(scale > 0)
+            scale--;
+    } else{
+        if(scale < maxScale)
+            scale++;
+
+        if(IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+            drawCol = { (unsigned char)(col.r - 30), col.g, col.b, col.a } ;
+
+        if(IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+            isTouch = true;
+    }
+
+    drawRectangle = {rectangle.x - scale + camera.x, rectangle.y - scale + camera.y, rectangle.width + scale * 2, rectangle.height + scale * 2};
+
+    DrawRectanglePro({drawRectangle.x - shadowScale, drawRectangle.y - shadowScale,
+                      drawRectangle.width + shadowScale * 2, drawRectangle.height + shadowScale * 2},
+                     {0,0}, 0, {0,0,0, (unsigned char)(100 * ((float)scale / maxScale))});
+
+    DrawRectanglePro(drawRectangle, {0, 0}, 0, drawCol);
+
+    TextSize = {(float)((text.size()) * 20) , 40};
+
+    DrawTextPro(TextFont, text.c_str(), {startPos.x + camera.x, startPos.y + camera.y}, {TextSize.x / 2, TextSize.y / 2},0, fontSize, 3, BLACK);
+}
---
+++ RayLibShowcase11/src/Button.h
@@ -0,0 +1,39 @@
+//
+// Created by tikho on 03.05.2023.
+//
+
+#ifndef RAYLIBSHOWCASE_BUTTON_H
+#define RAYLIBSHOWCASE_BUTTON_H
+
+#include <raylib.h>
+#include "string"
+
+class Button {
+    Rectangle rectangle, drawRectangle;
+
+    int scale = 0, fontSize;
+
+    const int maxScale = 5;
+
+    Color col;
+
+    const int shadowScale = 5;
+
+    Font TextFont;
+
+    Vector2 startPos {0,0};
+
+    Vector2 TextSize;
+
+public:
+    Button(Rectangle, std::string, Font, int = 40, Color = RED);
+
+    void update(Vector2 camera = {0, 0});
+
+    bool isTouch = false;
+
+    std::string text;
+};
+
+
+#endif //RAYLIBSHOWCASE_BUTTON_H
