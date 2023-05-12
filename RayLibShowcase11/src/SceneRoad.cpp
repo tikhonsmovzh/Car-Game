ea263c6f75b151bf66b90b5994d605aa16601bb0
netiosnape <snape2288@gmail.com>
2023-05-12 18:35:50 +0300

    debug road

--- RayLibShowcase11/src/SceneRoad.cpp
+++ RayLibShowcase11/src/SceneRoad.cpp
@@ -7,26 +7,68 @@
 SceneRoad::SceneRoad(int num) : Scene(num) {}
 
 void SceneRoad::update() {
-    if(len < maxlen){
-        side = GetRand(NowPos, boolMatrix);
-        //side = GetRandomValue(1, 4);
-        if(side == 1 && oldSide != 3)   NowPos.y = NowPos.y - 10;
-        if(side == 2 && oldSide != 4)   NowPos.x = NowPos.x + 10;
-        if(side == 3 && oldSide != 1)   NowPos.y = NowPos.y + 10;
-        if(side == 4 && oldSide != 2)   NowPos.x = NowPos.x - 10;
-        road.push_back(NowPos);
-        boolMatrix[int(NowPos.y/10)][int(NowPos.x/10)] = true;
-        for(int i = 0;i < road.size(); i++){
-            DrawRectangle(road[i].x, road[i].y, 10, 10, BLACK);
-        }
-        DrawRectangle(NowPos.x, NowPos.y, 10, 10, RED);
-        oldSide = side;
-        len++;
+    //for(int i;
+    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+        full_generate();
     }
+    draw();
 }
 
 void SceneRoad::Load() {
-    road.push_back(NowPos);
+    full_generate();
+}
+
+void SceneRoad::UnLoad() {
+
+}
+
+Vector2 SceneRoad::GetRand(Vector2 NowPos, bool** boolMatrix, int countSide) {
+    side2 = GetRandomValue(1, 4);
+    if(countSide > 20){
+        reload();
+        return sideAndCount;
+    }
+    if(!boolFirst){
+        firstSide = side2;
+        boolFirst = true;
+        sideAndCount.x = firstSide;
+        std::cout<< firstSide << "\n";
+        sideAndCount.y = 0;
+        return sideAndCount;
+    }
+    //std::cout << side << ' ' << int((NowPos.y-10)/10) << ' ' << int(NowPos.x+10)/10 << '\n';
+    if(side2 == 1 && (boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y-20)/10)][int(NowPos.x)/10])){
+        countSide++;
+        sideAndCount = GetRand(NowPos, boolMatrix, countSide);
+        countSide = sideAndCount.y;
+    }
+    else if(side2 == 2 && (boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y)/10)][int(NowPos.x+20)/10])){
+        countSide++;
+        sideAndCount = GetRand(NowPos, boolMatrix, countSide);
+    }
+    else if(side2 == 3 && (boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y+20)/10)][int(NowPos.x)/10])){
+        countSide++;
+        sideAndCount = GetRand(NowPos, boolMatrix, countSide);
+    }
+    else if(side2 == 4 && (boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y)/10)][int(NowPos.x-20)/10])){
+        countSide++;
+        sideAndCount = GetRand(NowPos, boolMatrix, countSide);
+    }
+    else {
+        countSide = 0;
+    }
+    sideAndCount = {(float)side2, (float)countSide};
+    return sideAndCount;
+}
+
+void SceneRoad::reload() {
+    boolEnd = false;
+    boolFirst = false;
+    len = 0;
+    for(int i = road.size(); i > 0;i--)
+    {
+        road.pop_back();
+    }
     boolMatrix = new bool*[85];
     for (int i = 0; i < 85; i++) {
         boolMatrix[i] = new bool[150];
@@ -36,26 +78,108 @@ void SceneRoad::Load() {
             boolMatrix[i][j] = false;
         }
     }
+    boolMatrix[scale/2][scale/2] = true;
+    for(int i = 0;i < scale; i++){
+        boolMatrix[0][i] = true;
+        boolMatrix[scale-1][i] = true;
+    }
+    for(int i = 0;i < scale; i++){
+        boolMatrix[i][0] = true;
+        boolMatrix[i][scale-1] = true;
+    }
+    road.push_back(Start);
+    NowPos = Start;
 }
 
-void SceneRoad::UnLoad() {
-
+void SceneRoad::generate() {
+    sideAndCount = GetRand(NowPos, boolMatrix, countSide);
+    side = sideAndCount.x;
+    //side = GetRandomValue(1, 4);
+    if(side == 1 && oldSide != 3)   NowPos.y = NowPos.y - 10;
+    if(side == 2 && oldSide != 4)   NowPos.x = NowPos.x + 10;
+    if(side == 3 && oldSide != 1)   NowPos.y = NowPos.y + 10;
+    if(side == 4 && oldSide != 2)   NowPos.x = NowPos.x - 10;
+    road.push_back(NowPos);
+    boolMatrix[int(NowPos.y/10)][int(NowPos.x/10)] = true;
+//        for(int i = 0;i < road.size(); i++){
+//            DrawRectangle(road[i].x, road[i].y, 10, 10, BLACK);
+//        }
+//        DrawRectangle(NowPos.x, NowPos.y, 10, 10, RED);
+    oldSide = side;
+    len++;
+    //=i;
 }
 
-int SceneRoad::GetRand(Vector2 NowPos, bool** boolMatrix) {
-    side2 = GetRandomValue(1, 4);
-    std::cout << side << ' ' << int((NowPos.y-10)/10) << ' ' << int(NowPos.x+10)/10 << '\n';
-    if(side2 == 1 && (boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y-20)/10)][int(NowPos.x)/10])){
-        side2 = GetRand(NowPos, boolMatrix);
+void SceneRoad::draw() {
+//    for(int i = 0;i < road.size(); i++){
+//        DrawRectangle(road[i].x, road[i].y, 10, 10, BLACK);
+//    }
+    for (int i = 0; i < 85; ++i) {
+        for (int j = 0; j < 150; ++j) {
+            if(boolMatrix[i][j]){
+                DrawRectangle(i*10,j*10,10,10,BLACK);
+            }
+        }
     }
-    if(side2 == 2 && (boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y)/10)][int(NowPos.x+20)/10])){
-        side2 = GetRand(NowPos, boolMatrix);
+    //DrawRectangle(NowPos.x, NowPos.y, 10, 10, RED);
+}
+
+void SceneRoad::full_generate() {
+    reload();
+    while(!boolEnd){
+        if(len < maxlen)
+            //=;i++)
+        {
+            if(firstSide == 0){
+                generate();
+            }
+            else if(firstSide == 1 && ((NowPos.x == Start.x - 20 && NowPos.y == Start.y) || (NowPos.x == Start.x + 20 && NowPos.y == Start.y) || (NowPos.x == Start.x && NowPos.y == Start.y - 20))&&minLen < len && !boolEnd){
+                draw();
+                boolEnd = true;
+                std::cout << firstSide << " end" << "\n";
+                if(Start.x == NowPos.x){
+                    boolMatrix[(int)(Start.y+NowPos.y)/20][(int)Start.x/10] = true;
+                }
+                else{
+                    boolMatrix[(int)Start.y/10][(int)(Start.x+NowPos.x)/20] = true;
+                }
+            }
+            else if(firstSide == 2 && ((NowPos.x == Start.x && NowPos.y == Start.y + 20) || (NowPos.x == Start.x + 20 && NowPos.y == Start.y) || (NowPos.x == Start.x && NowPos.y == Start.y - 20))&&minLen < len && !boolEnd){
+                draw();
+                boolEnd = true;
+                std::cout << firstSide << " end" << "\n";
+                if(Start.x == NowPos.x){
+                    boolMatrix[(int)(Start.y+NowPos.y)/20][(int)Start.x/10] = true;
+                }
+                else{
+                    boolMatrix[(int)Start.y/10][(int)(Start.x+NowPos.x)/20] = true;
+                }
+            }
+            else if(firstSide == 3 && ((NowPos.x == Start.x - 20 && NowPos.y == Start.y) || (NowPos.x == Start.x + 20 && NowPos.y == Start.y) || (NowPos.x == Start.x && NowPos.y == Start.y + 20))&&minLen < len && !boolEnd){
+                draw();
+                boolEnd = true;
+                std::cout << firstSide << " end" << "\n";
+                if(Start.x == NowPos.x){
+                    boolMatrix[(int)(Start.y+NowPos.y)/20][(int)Start.x/10] = true;
+                }
+                else{
+                    boolMatrix[(int)Start.y/10][(int)(Start.x+NowPos.x)/20] = true;
+                }
+            }
+            else if(firstSide == 4 && ((NowPos.x == Start.x && NowPos.y == Start.y + 20) || (NowPos.x == Start.x - 20 && NowPos.y == Start.y) || (NowPos.x == Start.x && NowPos.y == Start.y - 20))&&minLen < len && !boolEnd){
+                draw();
+                boolEnd = true;
+                std::cout << firstSide << " end" << "\n";
+                if(Start.x == NowPos.x){
+                    boolMatrix[(int)(Start.y+NowPos.y)/20][(int)Start.x/10] = true;
+                }
+                else{
+                    boolMatrix[(int)Start.y/10][(int)(Start.x+NowPos.x)/20] = true;
+                }
+            }
+            else{
+                generate();
+            }
+        }
     }
-    if(side2 == 3 && (boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x+10)/10] || boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y+20)/10)][int(NowPos.x)/10])){
-        side2 = GetRand(NowPos, boolMatrix);
-    }
-    if(side2 == 4 && (boolMatrix[int((NowPos.y-10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y+10)/10)][int(NowPos.x-10)/10] || boolMatrix[int((NowPos.y)/10)][int(NowPos.x-20)/10])){
-        side2 = GetRand(NowPos, boolMatrix);
-    }
-    return side2;
 }
--- RayLibShowcase11/src/SceneRoad.h
+++ RayLibShowcase11/src/SceneRoad.h
@@ -14,13 +14,19 @@ public:
     void update();
     void Load();
     void UnLoad();
-    int len, maxlen = 19999999, side, oldSide, side2;
+    int len, maxlen = 999999,minLen = 15, side, oldSide, side2, countSide = 0, firstSide = 0;
+    bool boolFirst = false, boolEnd = false;
     std::vector<Vector2> road;
     bool** boolMatrix;
     void updateMatrix(bool** matrix){}
+    const int scale =80;
+    Vector2 NowPos ={(float)scale*5, (float)scale*5}, Start ={(float)scale*5, (float)scale*5}, sideAndCount = {0,0};
+    void reload();
+    void generate();
+    void draw();
+    void full_generate();
 
-    Vector2 NowPos = { 500,500};
-    int GetRand(Vector2 NowPos, bool** boolMatrix);
+    Vector2 GetRand(Vector2 NowPos, bool** boolMatrix, int countSide);
 };
 
 
